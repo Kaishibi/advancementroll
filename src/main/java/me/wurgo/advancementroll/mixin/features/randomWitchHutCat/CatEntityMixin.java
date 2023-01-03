122e206227783ed9b30b50eb67ca6ce9bbced3f1
wurgo <laxvideoeffects@gmail.com>
2023-01-03 17:50:59 +0000

    add randomWitchHutCat

---
+++ src/main/java/me/wurgo/advancementroll/mixin/features/randomWitchHutCat/CatEntityMixin.java
@@ -0,0 +1,15 @@
+package me.wurgo.advancementroll.mixin.features.randomWitchHutCat;
+
+import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.structure.StructureStart;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(CatEntity.class)
+public class CatEntityMixin {
+    @Redirect(method = "initialize", at = @At(value = "INVOKE", target = "Lnet/minecraft/structure/StructureStart;hasChildren()Z"))
+    private boolean randomCat(StructureStart<?> instance) {
+        return false;
+    }
+}
