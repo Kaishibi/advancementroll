dbdefba9520cd0e8cd70dc185d68c6b558b00ceb
wurgo <laxvideoeffects@gmail.com>
2023-01-03 18:49:16 +0000

    add singularPandaSpawns

--- src/main/java/me/wurgo/advancementroll/Advancementroll.java
+++ src/main/java/me/wurgo/advancementroll/Advancementroll.java
@@ -2,17 +2,9 @@ package me.wurgo.advancementroll;
 
 import com.google.gson.GsonBuilder;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.ModContainer;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.resource.ResourceImpl;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Advancementroll implements ClientModInitializer {
     public static int screenSeed = 0;
